218685b3b5538d4718d97d80026b1200f708fc8a
mmmparlan <126334861+mmmparlan@users.noreply.github.com>
2023-04-06 15:29:26 -0400

    re-added all files relating to personal website

--- javascript.js
+++ javascript.js
@@ -1,3 +1,32 @@
 // function addBackgroundImage {
 //     document.body.style.backgroundImage = "url('compass.jpg')";
 // }
+function scaleUpPOL(){
+    var itemToScale = document.getElementById('projectOneLine');
+    itemToScale.style.transform = 'scale(1.1)';
+}
+
+function scaleDwnPOL(){
+    var itemToScale = document.getElementById('projectOneLine');
+    itemToScale.style.transform = 'scale(1)';
+}
+
+function scaleUpPTL(){
+    var itemToScale = document.getElementById('projectTwoLine');
+    itemToScale.style.transform = 'scale(1.1)';
+}
+
+function scaleDwnPTL(){
+    var itemToScale = document.getElementById('projectTwoLine');
+    itemToScale.style.transform = 'scale(1)';
+}
+
+function scaleUpPThreeL(){
+    var itemToScale = document.getElementById('projectThreeLine');
+    itemToScale.style.transform = 'scale(1.1)';
+}
+
+function scaleDwnPThreeL(){
+    var itemToScale = document.getElementById('projectThreeLine');
+    itemToScale.style.transform = 'scale(1)';
+}
